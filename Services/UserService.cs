5b300f3bc8c473315f4e607b3a0ffc804b3fb1d6
Gareth <gareth@redovoting.io>
2025-09-29 11:32:00 -0400

    Added user service and interface for userservice

---
+++ Services/UserService.cs
@@ -0,0 +1,15 @@
+﻿using SocialNetworkV1.Models;
+
+namespace SocialNetworkV1.Services
+{
+    public interface IUserService 
+    {
+        User? GetUser(string username);
+        User CreateUser(string username);
+        void UpdateUser(User user);
+        void DeleteUser(User user);
+    }
+    public class UserService
+    {
+    }
+}
