ef3ff511c3ebaa2e29a6c896a791a2019b9e5c82
Gareth <gareth@redovoting.io>
2025-09-29 14:29:56 -0400

    Removed weather stuff and added entity with npgsql connection

---
+++ Data/UserDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNetworkV1.Models;
+
+namespace SocialNetworkV1.Data
+{
+    public class UserDb: DbContext
+    {
+        public UserDb(DbContextOptions options): base(options) { }
+        public DbSet<User> users { get; set; } = null!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SocialNetworkV1.Data;
+using SocialNetworkV1.Models;
 using SocialNetworkV1.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<UserDb>(options => options.UseNpgsql("localhost;Port=5432;Database=userdb;Username=postgres;Password=postgres"));
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IUserService, UserService>();
 
