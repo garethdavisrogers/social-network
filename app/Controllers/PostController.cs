af705c87ed215585bbaf7942698ac13cd8e22a42
Gareth <gareth@redovoting.io>
2025-12-09 11:17:26 -0500

    Added post controller and service

---
+++ app/Controllers/PostController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialNetworkV1.Models;
+using SocialNetworkV1.Services;
+using SocialNetworkV1.DTOs.Requests.Users;
+using SocialNetworkV1.DTOs.Responses.Users;
+using SocialNetworkV1.DTOs.Requests.Auth;
+
+namespace SocialNetworkV1.Controllers
+{
+    [ApiController]
+    [Route("post")]
+    public class PostController : Controller
+    {
+        private readonly IAuthService _authService;
+        public PostController(IAuthService authService) => _authService = authService;
+
+        [HttpPost("CreatePost")]
+        public async Task<IActionResult> CreatePost([FromBody] CreateUserRequest req)
+        {
+            if (string.IsNullOrWhiteSpace(req.Email) || string.IsNullOrWhiteSpace(req.Password))
+            {
+                return BadRequest("Email, and Password are required.");
+            }
+            var (success, errors, user) = await _authService.RegisterUserAsync(req.Email, req.Password);
+            if (!success || user == null)
+            {
+                return BadRequest(new { errors });
+            }
+
+            var resp = new GetUserResponse(user.Id, user.Name, user.Email!, user.Posts);
+            return CreatedAtRoute("GetUserById", new { id = user.Id }, resp);
+        }
+
+    }
+}
---
+++ app/Services/PostService.cs
@@ -0,0 +1,29 @@
+﻿using SocialNetworkV1.Models;
+using SocialNetworkV1.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace SocialNetworkV1.Services
+{
+    public interface IPostService
+    {
+        Task<(bool Success, IEnumerable<string> Errors, Post? post)> CreatePostAsync(Guid userId, string body);
+    }
+    public class PostService : IPostService
+    {
+        private readonly UserManager<User> _userManager;
+
+        public PostService(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        public async Task<(bool Success, IEnumerable<string> Errors, Post? post)> CreatePostAsync(Guid userId, string body)
+        {
+            var post = new Post(userId, body);
+
+            return (true, Enumerable.Empty<string>(), post);
+        }
+
+    }
+}
